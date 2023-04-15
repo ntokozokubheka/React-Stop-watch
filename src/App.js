e48957f2feb43c212792a42f2ece0aab628b45c9
Dairenji <37531253+ntokozokubheka@users.noreply.github.com>
2023-04-15 14:27:34 +0000

    Changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
 import './App.css';
+import React, {useState} from 'react';
+import DisplayComponent from './DisplayComponent';
+import BtnComponent from './BtnComponent';
 
 function App() {
+const [time ,setTime]=useState({ms:0, s:0, m:0,h:0});
+const [interv,setInterv] =useState();
+const [status,setStatus] =useState(0);
+
+const start =()=>{
+
+  run();
+  setStatus(1);
+  setInterv(setInterval(run, 10));
+}
+
+
+var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+
+const run = () => {
+  if(updatedM === 60){
+    updatedH++;
+    updatedM = 0;
+  }
+  if(updatedS === 60){
+    updatedM++;
+    updatedS = 0;
+  }
+  if(updatedMs === 100){
+    updatedS++;
+    updatedMs = 0;
+  }
+  updatedMs++;
+  return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
+};
+const stop = () => {
+  clearInterval(interv);
+  setStatus(2);
+};
+
+const reset = () => {
+  clearInterval(interv);
+  setStatus(0);
+  setTime({ms:0, s:0, m:0, h:0})
+};
+
+const resume = () => start();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="clock-holder">
+      <div className="main-section">
+        <div className="stop-watch">
+
+
+         <DisplayComponent time={time}/>
+         <BtnComponent status={status} resume={resume} reset={reset} stop={stop} start={start} />
+
+        </div>
+      </div>     
     </div>
   );
 }
